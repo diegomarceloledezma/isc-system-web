1f342cae99cb3519e85acc8f885415bcaa07f5ca
Diego Ledezma <diegomarceloledezma@gmail.com>
2024-09-16 01:57:41 -0500

    cypress.config_upload

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    USERNAME:"68779",
+    PASSWORD:"Diego0803",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
